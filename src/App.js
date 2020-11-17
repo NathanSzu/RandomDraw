25cd7ceea37f86c3a11872a50b4c4507d1efcf59
Nathan Szurek <nathan.szurek@yahoo.com>
2020-11-16 19:16:39 -0600

    Added firebase sdk, firebase-react-hook, auth, and firestore references

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
+// Importing firebase SDK
+import firebase from 'firebase/app';
+// Importing firestore database
+import 'firebase/firestore';
+// Importing firebase user authentication
+import 'firebase/auth';
 
+// Importing firebase hooks to use in react
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  // Firebase config data goes here.
+  apiKey: "AIzaSyComTYk2QyWd4iJzK7ReXBL2azGd-Vo5gA",
+  authDomain: "randomdraw-264a2.firebaseapp.com",
+  databaseURL: "https://randomdraw-264a2.firebaseio.com",
+  projectId: "randomdraw-264a2",
+  storageBucket: "randomdraw-264a2.appspot.com",
+  messagingSenderId: "449384473316",
+  appId: "1:449384473316:web:fe9013c6557d6d627002f1"
+})
+
+// Referencing auth and firestore as global variables
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+    // Stores info of currently logged in user as an object
+    const [user] = useAuthState(auth);
+    
   return (
     <div className="">
       <header className="">
