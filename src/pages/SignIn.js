54b1b411ac1a8765e51ec607d7ab00a10d6507a0
Nathan Szurek <nathan.szurek@yahoo.com>
2020-11-20 18:35:37 -0600

    Created basic login form

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -3,7 +3,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 export default function SignIn({ auth }) {
-    
+
     const signInWithGoogle = () => {
         // Instantiate Google auth provider
         const provider = new firebase.auth.GoogleAuthProvider();
@@ -11,6 +11,22 @@ export default function SignIn({ auth }) {
     }
 
     return (
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
+        <main className='col-md-4 ml-auto mr-auto'>
+            <form>
+                <label htmlFor='email'>
+                    LALA
+                </label>
+                <input className='w-100' id='email' type='email'>
+                </input>
+                <label htmlFor='password'>
+                    LALA
+                </label>
+                <input className='w-100' id='password' type='password'>
+                </input>
+                <button className='w-100 mt-2' type='submit' value='login'>Login</button>
+            </form>
+            <button className='w-50 mt-3' onClick={signInWithGoogle}>Sign in with Google</button>
+            {/* <button className='w-50 mt-3' onClick={signInWithGoogle}>Sign in with Facebook</button> */}
+        </main>
     )
 }
